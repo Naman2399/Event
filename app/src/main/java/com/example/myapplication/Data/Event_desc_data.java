2a065f1344b6e2c52646b5b11fab4075f3da5c3d
Naman2399 <namanmalpani2399@gmail.com>
2019-12-26 11:35:25 +0530

    Champ begins

--- app/src/main/java/com/example/myapplication/Data/Event_desc_data.java
+++ app/src/main/java/com/example/myapplication/Data/Event_desc_data.java
@@ -1,4 +1,44 @@
 package com.example.myapplication.Data;
 
 public class Event_desc_data {
+    private String event_name , date , desc , time ;
+
+    public Event_desc_data(String event_name, String date, String desc, String time) {
+        this.event_name = event_name;
+        this.date = date;
+        this.desc = desc;
+        this.time = time;
+    }
+
+    public String getEvent_name() {
+        return event_name;
+    }
+
+    public void setEvent_name(String event_name) {
+        this.event_name = event_name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
--- app/src/main/java/com/example/myapplication/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/myapplication/ui/gallery/GalleryFragment.java
@@ -6,15 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.Data.Event_desc_data;
 import com.example.myapplication.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -23,11 +23,12 @@ import com.google.firebase.database.Query;
 
 public class GalleryFragment extends Fragment {
 
+    // Firebase
     private FirebaseAuth mAuth ;
     private DatabaseReference mDatabase ;
     private RecyclerView recyclerView ;
     private Query query ;
-
+    private String post_key , date , desc , time ;
     private GalleryViewModel galleryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -36,13 +37,16 @@ public class GalleryFragment extends Fragment {
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
 
+
         // Firebase
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser mUser = mAuth.getCurrentUser();
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("event_1");
+        String uid  = mUser.getUid();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Planning");
         //Querry
         query = FirebaseDatabase.getInstance().getReference().child("Planning");
-        recyclerView = (RecyclerView) root.findViewById(R.id.recyclerid);
+        //Recycler View
+        recyclerView = (RecyclerView)root.findViewById(R.id.recyclerid);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setReverseLayout(true);
         layoutManager.setStackFromEnd(true);
@@ -52,7 +56,44 @@ public class GalleryFragment extends Fragment {
     }
 
     @Override
-    public void onStart() {
+    public void onStart(){
         super.onStart();
+        FirebaseRecyclerAdapter<Event_desc_data, MyViewHolder> adapter = new FirebaseRecyclerAdapter<Event_desc_data, MyViewHolder>(
+                Event_desc_data.class,
+                R.layout.event_discription_block,
+                MyViewHolder.class,
+                mDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(MyViewHolder myViewHolder, final Event_desc_data event_desc_data, final int i) {
+                myViewHolder.setDate(event_desc_data.getDate());
+                myViewHolder.setName(event_desc_data.getDesc());
+                myViewHolder.setText(event_desc_data.getTime());
+            }
+        };
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+        public void setDate(String date){
+            TextView mDate = mView.findViewById(R.id.date);
+            mDate.setText(date);
+        }
+        public void setName(String name){
+            TextView mName = mView.findViewById(R.id.event_name);
+            mName.setText(name);
+        }
+        public void setText(String task){
+            TextView mTask = mView.findViewById(R.id.event_desc);
+            mTask.setText(task);
+        }
+
     }
 }
