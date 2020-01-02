ef3e5b775a52cefd10c8b3edf23f68d4cc429f26
Naman2399 <namanmalpani2399@gmail.com>
2020-01-02 09:09:22 +0530

    Champ begins

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +9,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.myapplication.CustomSwipeAdapter;
 import com.example.myapplication.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    ViewPager viewPager ;
+    CustomSwipeAdapter customSwipeAdapter ;
+    private Timer timer;
+    private int current_position=0;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -21,6 +31,30 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+//        viewPager = root.findViewById(R.id.view_pager);
+//        customSwipeAdapter = new CustomSwipeAdapter(getActivity());
+//        viewPager.setAdapter(customSwipeAdapter);
+//        createSlideShow();
         return root;
     }
+
+    public void createSlideShow(){
+        final Handler handler=new Handler();
+        final Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                current_position=current_position%5;
+                viewPager.setCurrentItem(current_position++,true);
+
+            }
+        };
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+            }
+        },250,2500);
+    }
+
 }
